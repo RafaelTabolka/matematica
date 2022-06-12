507da2fe8e2720c4d9a3d7bcb2ed919ec42b295a
RafaelTabolka <rafaeltabolka@hotmail.com>
2022-06-11 22:42:10 -0300

    Adicionando laço for

--- src/Matematica.java
+++ src/Matematica.java
@@ -10,46 +10,34 @@ public class Matematica {
         try {
             Figura classeRetangulo = new Retangulo("Retângulo", "Verde", -10, 12);
             listaFigura.add(classeRetangulo);
-            System.out.println("Área do " + classeRetangulo.getNome() + " é:" + classeRetangulo.calcularArea());
-        }catch (FiguraException | RuntimeException e) {
+        } catch (FiguraException | RuntimeException e) {
             System.out.println("Erro: " + e.getMessage());
-        }finally {
+        } finally {
             System.out.println("Primeiro try/catch");
         }
 
         try {
             Figura classeCirculo = new Circulo("Círculo", "Vermelho", 4);
             listaFigura.add(classeCirculo);
-            System.out.println("Área do " + classeCirculo.getNome() + " é:" + classeCirculo.calcularArea());
-        }catch (FiguraException | RuntimeException e) {
+        } catch (FiguraException | RuntimeException e) {
             System.out.println(e.getMessage());
-        }finally {
+        } finally {
             System.out.println("Segundo try/catch");
         }
 
         try {
             Figura classeTriangulo = new Triangulo("Triângulo", "Amarelo", 20, 14);
             listaFigura.add(classeTriangulo);
-            System.out.println("Área do " + classeTriangulo.getNome() + " é:" + classeTriangulo.calcularArea());
-        }catch(FiguraException | RuntimeException e){
+        } catch (FiguraException | RuntimeException e) {
             System.out.println(e.getMessage());
-        }finally {
+        } finally {
             System.out.println("Terceito try/catch");
         }
 
+        for (Figura x : listaFigura) {
+            System.out.println("A figura " + x.getNome() + " tem área igual a: " + String.format("%.3fmm²%n",x.calcularArea()));
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
